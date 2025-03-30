4ffe76d12b22ae1b20e4ccb243939f8e002a8a3e
Rasio09 <rasiovolume22@gmail.com>
2025-03-30 18:20:53 +0700

    update halaman about-dev 2

--- about-dev.php
+++ about-dev.php
@@ -27,11 +27,22 @@ require("header.php");
             <p>about meeabout meeabout meeabout meeabout meeabout mee...</p>
         </div>
         <h3>EDUCATION</h3>
-        <p>about meeabout meeabout meeabout meeabout meeabout...</p>
-        <h3>RESEARCH</h3>
+        <p style="font-weight: bold;">FRESH GRADUATE S1 JURUSAN TEKNIK INFORMATIKA</p>
+        <p style="font-weight: bold;">UNIVERSITAS NUSANTARA PGRI KEDIRI</p>
+        <p style="font-weight: bold;">IPK 3.85</p>
+        <p></p>
+        <h3>RESEARCH (Penelitian)</h3>
         <div class="certificates-dev">
-            <div class="certificate-dev">about meeabout meeabout meeabout...</div>
-            <div class="certificate-dev">about meeabout meeabout meeabout...</div>
+            <div class="certificate-dev">
+                <p>1</p>
+                <p>PENELITIAN PUBLIKASI JURNAL INTERNASIONAL</p>
+                <p>ICSINTESA</p>
+            </div>
+            <div class="certificate-dev">
+                <p>2</p>
+                <p>PENELITIAN PUBLIKASI JURNAL NASIONAL</p>
+                <p>STAINS</p>
+            </div>
             <div class="certificate-dev">about meeabout meeabout meeabout...</div>
         </div>
         <h3>SERTIFIKASI</h3>
@@ -55,15 +66,16 @@ require("header.php");
         <div class="section-dev">
             <h3>EXPERIENCE AND SKILL</h3>
             <div class="experience-icons-dev">
-                <img src="html.png" alt="HTML">
-                <img src="css.png" alt="CSS">
-                <img src="javascript.png" alt="JavaScript">
-                <img src="php.png" alt="PHP">
-                <img src="python.png" alt="Python">
-                <img src="office.png" alt="Microsoft Office">
-                <img src="mysql.png" alt="MySQL">
-                <img src="tableau.png" alt="Tableau">
-                <img src="flask.png" alt="Flask">
+                <img src="img/skill/html.png" alt="HTML">
+                <img src="img/skill/css.png" alt="CSS">
+                <img src="img/skill/js.png" alt="JavaScript">
+                <img src="img/skill/php.png" alt="PHP"><br>
+                <img src="img/skill/python.png" alt="Python">
+                <img src="img/skill/office.png" alt="Microsoft Office">
+                <img src="img/skill/mysql.png" alt="MySQL"><br>
+                <img src="img/skill/tableau.png" alt="Tableau">
+                <img src="img/skill/flask.png" alt="Flask"><br>
+                <img src="img/skill/ml.png" alt="Flask">
             </div>
         </div>
     </div>
