14114cf3853efb0a6e2f1ac5533e233ecea211fe
Rasio09 <rasiovolume22@gmail.com>
2025-04-02 17:51:04 +0700

    update halaman about-dev finish

--- about-dev.php
+++ about-dev.php
@@ -28,51 +28,75 @@ require("header.php");
                 Saya <span style="font-weight:bold;">Rasio Fernandis</span> Fresh Graduate Jurusan Teknik Informatika dari <span style="font-weight:bold; font-style:italic;">Universitas Nusantara PGRI Kediri</span> dengan IPK 3.85 dan menguasai keahlian dalam Microsoft Office, pemrograman, dan basis data. Berpengalaman dalam membangun website sederhana mulai dari 0. Memiliki kemampuan berpikir kreatif dan mampu mengolah waktu secara efektif, mampu berkolaborasi dengan tim dan terbuka untuk terus belajar. Terbiasa bekerja part time pada saat kuliah menjadi guru bantu dan juga freelance sebagai data entry dan developer website.
             </p>
         </div>
-        <h2>EDUCATION</h2>
-        <p style="font-weight: bold; font-style: italic;">FRESH GRADUATE S1 JURUSAN TEKNIK INFORMATIKA</p>
-        <p style="font-weight: bold; font-style: italic;">UNIVERSITAS NUSANTARA PGRI KEDIRI</p>
-        <p style="font-weight: bold; font-style: italic;">IPK 3.85</p>
-        <p></p>
+        <div class="section-dev">
+            <h2>EDUCATION (PENDIDIKAN)</h2>
+            <br>
+            <p style="font-weight: bold; font-style: italic;">FRESH GRADUATE S1 JURUSAN TEKNIK INFORMATIKA</p>
+            <p style="font-weight: bold; font-style: italic;">UNIVERSITAS NUSANTARA PGRI KEDIRI</p>
+            <p style="font-weight: bold; font-style: italic;">IPK 3.85</p>
+            <p></p>
+        </div>
         <h2>RESEARCH (Penelitian)</h2>
         <div class="certificates-dev">
             <div class="certificate-dev">
-                <p>1</p>
-                <p>PENELITIAN PUBLIKASI JURNAL INTERNASIONAL (indeks SCOPUS)</p>
+                <p style="font-weight: bold;">1</p>
+                <p>PENELITIAN PUBLIKASI JURNAL INTERNASIONAL (Terindeks SCOPUS)</p>
                 <p>ICSINTESA</p>
-                <p style="text-align:center;">Judul Penelitian : <br> "Topic Modeling Using Latent Dirichlet Allocation Method Based On Child Anecdotal Record Data"</p>
+                <p style="text-align:center;">Judul Penelitian : <br> "TOPIC MODELING USING LATENT DIRICHLET ALLOCATION METHOD BASED ON CHILD ANECDOTAL RECORD DATA"</p>
                 <a href="https://ieeexplore.ieee.org/abstract/document/10747976" style="text-align:center;">Link Publikasi</a>
             </div>
+
             <div class="certificate-dev">
-                <p>2</p>
+                <p style="font-weight: bold;">2</p>
                 <p>PENELITIAN PUBLIKASI JURNAL NASIONAL</p>
                 <p>STAINS</p>
-                <p style="text-align:center;">Judul Penelitian : <br> "Implementasi Deteksi Bahasa Isyarat Tangan Menggunakan OpenCV dan MediaPipe"</p>
+                <p style="text-align:center;">Judul Penelitian : <br> "IMPLEMENTASI DETEKSI BAHASA ISYARAT TANGAN MENGGUNAKAN OpenCV DAN MediaPipe"</p>
                 <a href="https://proceeding.unpkediri.ac.id/index.php/stains/article/view/4337" style="text-align:center;">Link Publikasi</a>
             </div>
+
             <div class="certificate-dev">
-                <p>3</p>
+                <p style="font-weight: bold;">3</p>
                 <p>PENELITIAN SKRIPSI</p>
                 <p style="text-align:center;">Judul Penelitian : <br> "PENERAPAN LATENT DIRICHLET ALLOCATION PADA KLASIFIKASI ANAK TAMAN KANAK-KANAK (TK) BERDASARKAN CATATAN ANEKDOT"</p>
                 <a href="https://repository.unpkediri.ac.id/15159/" style="text-align:center;">Link Publikasi</a>
             </div>
         </div>
-        <h2>SERTIFIKASI</h2>
+        <h2>SERTIFIKASI PELATIHAN</h2>
         <div class="certificates-dev">
             <div class="certificate-dev">
-                <img src="sertif1.jpg" alt="Sertifikat 1">
+                <img src="img/sertif/web.png" alt="Sertifikat 1">
             </div>
             <div class="certificate-dev">
-                <img src="sertif2.jpg" alt="Sertifikat 2">
+                <img src="img/sertif/js.png" alt="Sertifikat 2">
             </div>
             <div class="certificate-dev">
-                <img src="sertif3.jpg" alt="Sertifikat 3">
+                <img src="img/sertif/html.png" alt="Sertifikat 2">
             </div>
             <div class="certificate-dev">
-                <img src="sertif4.jpg" alt="Sertifikat 4">
+                <img src="img/sertif/backend.png" alt="Sertifikat 3">
             </div>
             <div class="certificate-dev">
-                <img src="sertif5.jpg" alt="Sertifikat 5">
+                <img src="img/sertif/sql.png" alt="Sertifikat 4">
             </div>
+            <div class="certificate-dev">
+                <img src="img/sertif/ai.png" alt="Sertifikat 5">
+            </div>
+            <div class="certificate-dev">
+                <img src="img/sertif/data science.png" alt="Sertifikat 6">
+            </div>
+            <div class="certificate-dev">
+                <img src="img/sertif/python.png" alt="Sertifikat 7">
+            </div>
+            <div class="certificate-dev">
+                <img src="img/sertif/data.png" alt="Sertifikat 8">
+            </div>
+            <div class="certificate-dev">
+                <img src="img/sertif/data dq.png" alt="Sertifikat 8">
+            </div>
+            <div class="certificate-dev">
+                <img src="img/sertif/komputer.png" alt="Sertifikat 9">
+            </div>
+            
         </div>
         <div class="section-dev">
             <h2>My EXPERIENCE AND My SKILL</h2>
@@ -80,13 +104,13 @@ require("header.php");
                 <img src="img/skill/html.png" alt="HTML">
                 <img src="img/skill/css.png" alt="CSS">
                 <img src="img/skill/js.png" alt="JavaScript">
-                <img src="img/skill/php.png" alt="PHP"><br>
-                <img src="img/skill/python.png" alt="Python">
-                <img src="img/skill/office.png" alt="Microsoft Office">
+                <img src="img/skill/python.png" alt="Python"><br>
+                <img src="img/skill/flask.png" alt="Flask">
+                <img src="img/skill/php.png" alt="PHP">
                 <img src="img/skill/mysql.png" alt="MySQL"><br>
                 <img src="img/skill/tableau.png" alt="Tableau">
-                <img src="img/skill/flask.png" alt="Flask"><br>
-                <img src="img/skill/ml.png" alt="Flask">
+                <img src="img/skill/ml.png" alt="Machine Learning"><br>
+                <img src="img/skill/office.png" alt="Microsoft Office">
             </div>
         </div>
     </div>
