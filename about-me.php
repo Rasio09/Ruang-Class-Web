7a4813e082f38a5ff6f796eb631d011dc73fd3da
Rasio09 <rasiovolume22@gmail.com>
2025-04-04 09:17:40 +0700

    update halaman about me END

--- about-me.php
+++ about-me.php
@@ -14,8 +14,21 @@ require("header.php");
         </div>
         <div class="text-container">
             <h2>About Me Ruang Class Web</h2>
-            <p>Ruang Class Web adalah pelatihan atau course untuk pengetahuan anak dalam belajar pemrograman website sederhana. anak mulai umur 10 tahun ke atas.</p>
-            <a href="daftar.php" class="contact-btn">Daftar</a>
+            <p style="text-align:justify; text-indent: 30px;">Ruang Class Web adalah pelatihan atau course untuk pengetahuan anak dalam belajar mengembangkan pemrograman website sederhana. Serta mempersiapkan anak untuk menjadi seorang developer website baik FE (Front End) ataupun BE (Back End). Ruang Class Web ini diperuntukan anak mulai umur 10 tahun ke atas.</p>
+            <h4>BENEFIT : </h4>
+            <ul>
+                <li>Materi yang dirancang mudah dipahami oleh anak</li>
+                <li>Modul Materi</li>
+                <li>Kalau ada event lomba (bisa mendampingi)</li>
+                <li>Materi terbarukan</li>
+                <li>Implementasi AI (Artificial Intelegence)</li>
+            </ul>
+            <h4>Syarat Pendaftaran :</h4>
+            <ul>
+                <li>Wajib memiliki laptop sendiri</li>
+                <li>Mulai anak berumur 10 tahun keatas</li>
+            </ul>
+            <a href="daftar.php" class="contact-btn" style="margin-top: 20px; margin-left: 400px;">Segera Daftar</a>
         </div>
     </div>
 
@@ -25,8 +38,13 @@ require("header.php");
         </div>
         <div class="text-container">
             <h2>About Me Ruang Class Baca Tulis Hitung</h2>
-            <p>Ruang Class Baca Tulis Hitung adalah les-les an anak diluar jam sekolah dalam belajar baca tulis hitung. anak mulai umur 6 tahun ke atas.</p>
-            <a href="daftar.php" class="contact-btn">Daftar</a>
+            <p style="text-align:justify; text-indent: 30px;">Ruang Class Baca Tulis Hitung adalah les-les an diperuntukan anak-anak diluar jam sekolah untuk mengajarkan kepada anak membaca, menulis dan menghitung. Ruang Class Baca Tulis Hitung anak mulai umur 6 tahun ke atas atau anak yang sudah mulai memasuki jenjang pendidikan SD (Sekolah Dasar).</p>
+            <h4>Syarat Pendaftaran :</h4>
+            <ul>
+                <li>Anak berumur 6 tahun keatas</li>
+                <li>Anak sudah sekolah pada jenjang SD (Sekolah Dasar)</li>
+            </ul>
+            <a href="daftar.php" class="contact-btn" style="margin-top: 20px; margin-left: 400px;">Segera Daftar</a>
         </div>
     </div>
     <?php
